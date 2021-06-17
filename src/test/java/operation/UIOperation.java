adb51d5b3f5a094ea4778af90ceb6f9ff6fffba5
palerdos <palerdos95@gmail.com>
2021-06-17 12:45:01 +0200

    fix: restructured directory, added class for UI operations and added selenium & junit to dependencies

---
+++ src/test/java/operation/UIOperation.java
@@ -0,0 +1,11 @@
+package operation;
+
+import org.openqa.selenium.WebDriver;
+
+public class UIOperation {
+    WebDriver driver;
+
+    public UIOperation(WebDriver driver) {
+        this.driver = driver;
+    }
+}
