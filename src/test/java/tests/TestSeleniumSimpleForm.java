4f9f1f96201d754c99ffe75e420b83573fd39014
palerdos <palerdos95@gmail.com>
2021-06-19 22:43:54 +0200

    feat: simple form test created with POM

--- src/test/java/tests/TestSeleniumSimpleForm.java
+++ src/test/java/tests/TestSeleniumSimpleForm.java
@@ -4,25 +4,40 @@ import Pages.SeleniumSimpleForm;
 import Pages.SeleniumTestPage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestSeleniumSimpleForm {
 
     WebDriver driver = new ChromeDriver();
 
-    SeleniumSimpleForm objTestPage;
+    SeleniumSimpleForm objSimpleFormPage;
+    SeleniumTestPage objTestPage;
 
     @BeforeEach
     void setUp() {
         SeleniumTestHelper.setupHelper(driver);
-        objTestPage = new SeleniumSimpleForm(driver);
+        objSimpleFormPage = new SeleniumSimpleForm(driver);
+        objTestPage = new SeleniumTestPage(driver);
     }
 
     @AfterEach
     void tearDown() {
         driver.close();
         driver.quit();
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/singleFieldData.csv", numLinesToSkip = 1)
+    void singleFieldAndButton(String input, String expected) throws Exception {
+        objTestPage.clickInputFormMenu();
+        objTestPage.clickSimpleFormMenu();
+        objSimpleFormPage.enterInputMessage(input);
+        objSimpleFormPage.clickOnShowMessage();
+        assertEquals(expected, objSimpleFormPage.getDisplayedMessage());
     }
 
 }
